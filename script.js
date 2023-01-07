7b031290f071ab45e0ab338d925797c3a302f96d
Victor Hugo <victor.devfrontend@gmail.com>
2023-01-07 06:43:56 -0300

    project

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let navLinks = document.getElementById("navLinks");
 
 function showMenu() {
   navLinks.style.right = "0";
+  navLinks.style.display = "block";
 }
 
 function hideMenu() {
   navLinks.style.right = "-200px";
+  navLinks.style.display = "none";
 }
